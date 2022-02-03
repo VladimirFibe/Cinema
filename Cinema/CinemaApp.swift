dd2fee9c39d2a0e1894a16fbc0cf6d497d1f95b2
Vladimir Fibe <motiw@icloud.com>
2022-02-03 10:06:00 +0600

    Initial Commit

---
+++ Cinema/CinemaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CinemaApp.swift
+//  Cinema
+//
+//  Created by Vladimir Fibe on 03.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct CinemaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cinema/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cinema
+//
+//  Created by Vladimir Fibe on 03.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
